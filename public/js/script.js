38079f62507caf195fca16b8c880edc53b3e0457
Celso Lucas <lucascelso23@gmail.com>
2025-07-19 00:16:52 -0400

    animação de texto feita

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cursor = document.getElementById('cursor');
-    const trails = [];
-    const maxTrails = 100; // Número máximo de rastros
+const tl = gsap.timeline()
 
-    document.addEventListener('mousemove', (e) => {
-        // Atualiza a posição do cursor
-        cursor.style.left = `${e.clientX}px`;
-        cursor.style.top = `${e.clientY}px`;
+tl.from(".card_servicos", {
+    opacity: 0,
+    stagger: .3,
+    scrollTrigger: {
+        trigger: ".nossos_servicos",
+        start: "top 70%",
+        end: "bottom 90%",
+        scrub: 2
+    }
+})
 
-        // Cria um novo elemento de rastro
-        const trail = document.createElement('div');
-        trail.className = 'trail';
-        trail.style.left = `${e.clientX}px`;
-        trail.style.top = `${e.clientY}px`;
-        document.body.appendChild(trail);
-        trail.style.opacity = '1';
+const splitTypes = document.querySelectorAll(".textoAnimado");
 
-        // Adiciona ao array de rastros
-        trails.push(trail);
+splitTypes.forEach((char, i) => {
+    const text = new SplitType(char, { types: "words, chars" });
 
-        // Remove rastros antigos
-        if (trails.length > maxTrails) {
-            const oldTrail = trails.shift();
-            oldTrail.style.opacity = '0';
-            setTimeout(() => oldTrail.remove(), 500); // Remove após a transição
-        }
+    gsap.from(text.chars, {
+        scrollTrigger: {
+            trigger: char,
+            start: "top 90%",
+            end: "bottom 90%",
+            scrub: 1,
+        },
+        opacity: 0.05,
+        duration: 5,
+        stagger: 0.9
     });
 });
