d5d1803c211d59576cb7c4a5682f5d82270be6ee
Celso Lucas <lucascelso23@gmail.com>
2025-07-24 16:12:43 -0400

    commit

--- public/js/script.js
+++ public/js/script.js
@@ -7,13 +7,44 @@ tl.from(".card_servicos", {
         trigger: ".nossos_servicos",
         start: "top 70%",
         end: "bottom 90%",
+        scrub: 2,
+    }
+})
+
+tl.from(".item_contato", {
+    opacity: 0,
+    stagger: .3,
+    scrollTrigger: {
+        trigger: ".contato",
+        start: "top 70%",
+        end: "bottom 90%",
         scrub: 2
     }
 })
 
-const splitTypes = document.querySelectorAll(".textoAnimado");
+const splitTypes1 = document.querySelectorAll(".textoAnimadoQuemSomos");
 
-splitTypes.forEach((char, i) => {
+splitTypes1.forEach((char, i) => {
+    const text = new SplitType(char, { types: "words, chars" });
+
+    gsap.from(text.chars, {
+        scrollTrigger: {
+            trigger: char,
+            start: "top 65%",
+            end: "bottom 90%",
+            scrub: 2,
+            // markers: true
+        },
+        opacity: 0.05,
+        duration: 5,
+        stagger: 0.9
+    });
+});
+
+
+const splitTypes2 = document.querySelectorAll(".textoAnimado");
+
+splitTypes2.forEach((char, i) => {
     const text = new SplitType(char, { types: "words, chars" });
 
     gsap.from(text.chars, {
@@ -22,6 +53,7 @@ splitTypes.forEach((char, i) => {
             start: "top 90%",
             end: "bottom 90%",
             scrub: 1,
+            // markers: true
         },
         opacity: 0.05,
         duration: 5,
